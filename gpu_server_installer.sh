84da8be8767742772568d18c58b56b3c633f0768
Seikaijyu <2143026883@qq.com>
2025-05-30 17:42:23 +0800

    修正脚本

--- gpu_server_installer.sh
+++ gpu_server_installer.sh
@@ -2,10 +2,7 @@
 # 使用方式：在linux命令行或者wsl命令行输入 ./gpu_server_installer.sh
 # 回车即可
 git clone https://github.com/Seikaijyu/RWKV-PEFT
-git clone https://github.com/Seikaijyu/RWKV-PEFT-Simple
-mv RWKV-PEFT/* RWKV-PEFT-Simple/
-rm -rf ./RWKV-PEFT
-cd RWKV-PEFT-Simple
+mv RWKV-PEFT/* ../
 chmod +rwx ./*
 pip install -r requirements.txt
 pip install wandb
